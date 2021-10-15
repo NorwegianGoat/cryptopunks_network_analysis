d02bda2ebf375570eb20b4c42d2a6df864546a0c
NorwegianGoat <riccardomioli@gmail.com>
2021-10-15 18:28:58 +0200

    initial event parse, modified gitignore

--- main.py
+++ main.py
@@ -1,41 +1,100 @@
-from web3 import Web3
 import requests
 import json
+import time
+import os
+from web3 import Web3
 
-__ENDPOINT_URI = "https://cloudflare-eth.com/"
-__API_KEY = open('./api.key','r').readline()
-__FROM_BLOCK = 3914495
-__TO_BLOCK = 13422401
-__BLOCK_STEP = 10
+
+def get_last_block() -> int:
+    query = "https://api.etherscan.io/api?apikey=" + __API_KEY + "&module=block&action=getblocknobytime&timestamp=" + \
+            str(int(time.time())) + "&closest=before"
+    response = json.loads(requests.get(query).text)
+    print("Latest block is: ", response['result'])
+    return int(response['result'])
+
+
+__API_KEY = open('./api.key', 'r').readline()
+# __FROM_BLOCK = 3914495  # CryptoPunks creation block
+__FROM_BLOCK = 13423636
+__TO_BLOCK = get_last_block()
 
 
 def get_data():
-    __from = __FROM_BLOCK
-    for __to in range(__FROM_BLOCK, __TO_BLOCK, __BLOCK_STEP):
+    t0 = time.time()
+    start = __FROM_BLOCK
+    while (True):
         # Execute query
         query = "https://api.etherscan.io/api?apikey=" + __API_KEY + "&module=logs&action=getLogs&fromBlock=" + \
-            str(__from) + "&toBlock=" + str(__to) + \
+            str(start) + "&toBlock=latest" + \
             "&address=0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"
-        print(query, "\n REMAINING REQUESTS: ", (__TO_BLOCK - __from)/__BLOCK_STEP)
+        print(query, "\nREMAINING BLOCKS: ", (__TO_BLOCK - start))
         response = requests.get(query)
-        # Log error and re-execute the query
+        # Re-execute the query in case of error
         while response.status_code != 200:
-            error_string = "Request for block ", __from, " failed with code " + \
+            error_string = "Request for block ", start, " failed with code " + \
                 response.status_code + ". Retrying."
             print(error_string)
             response = requests.get(query)
-        response = json.loads(response.text)
         # Save data only in case there are events
+        response = json.loads(response.text)
+        to = int(response['result']
+                 [-1]['blockNumber'], 16)
         if (response['status'] == '1'):
-            with open("./data/" + str(__from) + "_" + str(__to) + ".json", 'w') as writer:
+            with open("./data/" + str(start) + "_" + str(to) + ".json", 'w') as writer:
                 print('Writing data from block ' +
-                      str(__from) + " to block " + str(__to))
+                      str(start) + " to block " + str(to))
                 json.dump(response['result'], writer, indent=4)
-        __from = __to + 1
+        ''' If start equals to to it means that there aren't events in the next 
+        block, so we break because we have retrieved all the events. Otherwise 
+        we have other events to process and we set the next block to query 
+        equals to the last we got, this happens because etherscan limits the
+        event retrieval to 1000 events and if we go to the next block we may
+        loose some events. '''
+        if (start == to):
+            t1 = time.time()
+            print('Data retrieval took:', t1 - t0, ' seconds.')
+            break
+        else:
+            start = to
 
-def clean_data(w3: Web3):
-    pass
+# TODO: Get event topic
+def parse_event(event: dict) -> dict:
+    event_type = event['topics'][0]
+    # Assign event
+    if event_type == '8a0e37b73a0d9c82e205d4d1a3ff3d0b57ce5f4d7bccf6bac03336dc101cb7ba':
+        # Topic 1 address:to, data punkIndex:uint256
+        event['topics'][0] = "Assign"
+        event['data'] = int(event['data'], 16)
+    #Transfer event 
+    elif event_type == 'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef':
+        #Topic 1 address:from, topic 2 address:to, data:value
+        event['topics'][0] = "Transfer"
+        event['data'] = int(event['data'], 16)
+    #PunkTransfer event
+    elif event_type == '05af636b70da6819000c49f85b21fa82081c632069bb626f30932034099107d8':
+        #Topic 1 address:from, topic 2 address:to, data:punkId
+        event['topics'][0] = "PunkTransfer"
+        event['data'] = int(event['data'], 16)
+    elif event_type == 'd':
+        pass
+    else:
+        # Uninteresting event
+        return None
+    return event
+
+# TODO: Write csv
+def refine_data():
+    # Merge all files in a single csv
+    downloaded_files = os.listdir('./data')
+    for file in downloaded_files:
+        with open('./data/' + file, 'r') as reader:
+            document = json.load(reader)
+            for event in document:
+                parsed_event = parse_event(event)
+                if(parsed_event):
+                    print(parsed_event)
 
 
 if __name__ == "__main__":
     get_data()
+    refine_data()

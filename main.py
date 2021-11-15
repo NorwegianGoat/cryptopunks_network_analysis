c04a7e739aeb7a94c835d91bf070abd97b2aa4c5
NorwegianGoat <riccardomioli@gmail.com>
2021-11-15 17:31:58 +0100

    Updated data and finished initial data filtering

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 import json
 import time
 import os
-from web3 import Web3
 
 
 def get_last_block() -> int:
@@ -16,6 +15,7 @@ def get_last_block() -> int:
 __API_KEY = open('./api.key', 'r').readline()
 __FROM_BLOCK = 3914495  # CryptoPunks creation block
 __TO_BLOCK = get_last_block()
+__NULL_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 
 def get_data():
@@ -43,9 +43,9 @@ def get_data():
                 print('Writing data from block ' +
                       str(start) + " to block " + str(to))
                 json.dump(response['result'], writer, indent=4)
-        ''' If start equals to to it means that there aren't events in the next 
-        block, so we break because we have retrieved all the events. Otherwise 
-        we have other events to process and we set the next block to query 
+        ''' If start equals to to it means that there aren't events in the next
+        block, so we break because we have retrieved all the events. Otherwise
+        we have other events to process and we set the next block to query
         equals to the last we got, this happens because etherscan limits the
         event retrieval to 1000 events and if we go to the next block we may
         loose some events. '''
@@ -56,44 +56,51 @@ def get_data():
         else:
             start = to
 
-# TODO: Get event topic
-def parse_event(event: dict) -> dict:
+
+# Event is returned in form of timestamp, event type, from, to, punkId
+def parse_event(event: dict) -> str:
     event_type = event['topics'][0]
-    # Assign event
-    if event_type == '8a0e37b73a0d9c82e205d4d1a3ff3d0b57ce5f4d7bccf6bac03336dc101cb7ba':
-        # Topic 1 address:to, data punkIndex:uint256
+    event['timeStamp'] = str(int(event['timeStamp'], 16))
+    # Assign(address indexed to, uint256 punkIndex);
+    if event_type == '0x8a0e37b73a0d9c82e205d4d1a3ff3d0b57ce5f4d7bccf6bac03336dc101cb7ba':
         event['topics'][0] = "Assign"
-        event['data'] = int(event['data'], 16)
-    #Transfer event 
-    elif event_type == 'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef':
-        #Topic 1 address:from, topic 2 address:to, data:value
-        event['topics'][0] = "Transfer"
-        event['data'] = int(event['data'], 16)
-    #PunkTransfer event
-    elif event_type == '05af636b70da6819000c49f85b21fa82081c632069bb626f30932034099107d8':
-        #Topic 1 address:from, topic 2 address:to, data:punkId
+        event['data'] = str(int(event['data'], 16))
+        event = event['timeStamp'] + ',' + event['topics'][0] + ',' + __NULL_ADDRESS + ',' + \
+            event['topics'][1] + ',' + event['data'] + '\n'
+    # PunkBought(uint indexed punkIndex, uint value, address indexed fromAddress, address indexed toAddress);
+    elif event_type == '0x58e5d5a525e3b40bc15abaa38b5882678db1ee68befd2f60bafe3a7fd06db9e3':
+        event['topics'][0] = "Punk Bought"
+        event['topics'][1] = str(int(event['topics'][1], 16))
+        event = event['timeStamp'] + ","+event['topics'][0] + "," + event['topics'][2] + \
+            "," + event['topics'][3] + "," + event['topics'][1] + '\n'
+    # PunkTransfer(address indexed from, address indexed to, uint256 punkIndex);
+    elif event_type == '0x05af636b70da6819000c49f85b21fa82081c632069bb626f30932034099107d8':
         event['topics'][0] = "PunkTransfer"
-        event['data'] = int(event['data'], 16)
-    elif event_type == 'd':
-        pass
+        event['data'] = str(int(event['data'], 16))
+        event = event['timeStamp'] + ","+event['topics'][0] + "," + event['topics'][1] + \
+            "," + event['topics'][2] + "," + event['data'] + '\n'
     else:
         # Uninteresting event
         return None
     return event
 
-# TODO: Write csv
-def refine_data():
+
+def filter_data():
     # Merge all files in a single csv
-    downloaded_files = os.listdir('./data')
-    for file in downloaded_files:
-        with open('./logs_data/' + file, 'r') as reader:
-            document = json.load(reader)
+    downloaded_files = os.listdir('./logs_data')
+    events = []
+    with open('./logs_data/logs.csv', 'w') as writer:
+        writer.write('timestamp, event_type, source, target, punk_id\n')
+        for file in downloaded_files:
+            with open('./logs_data/' + file, 'r') as reader:
+                document = json.load(reader)
             for event in document:
                 parsed_event = parse_event(event)
                 if(parsed_event):
-                    print(parsed_event)
+                    events.append(parsed_event)
+        writer.writelines(events)
 
 
 if __name__ == "__main__":
-    #get_data()
-    #refine_data()
+    # get_data()
+    filter_data()
